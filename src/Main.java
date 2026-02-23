646e8e060bd7af64679ac3ee6fc43e8739026e73
Ashutoshtiwari902 <ashutoshtiwari.9at@gmail.com>
2026-02-23 08:38:35 +0530

    UC4: Stored banner in array and used loop for printing

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+        printBanner();
+    }
+
+    /**
+     * Prints OOPS banner using String array and loop
+     */
+    public static void printBanner() {
+
+        // Store all banner lines inside an array
         String[] banner = {
-
-                String.join("", " *****   ", " *****   ", " *****   ", " ***** "),
-                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
-                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
-                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
-                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
-                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
-                String.join("", " *****   ", " *****   ", " *****   ", " ***** ")
-
+                String.join("  ", "OOOO", "OOOO", "PPPP", "SSSS"),
+                String.join("  ", "O  O", "O  O", "P   P", "S"),
+                String.join("  ", "O  O", "O  O", "PPPP", "SSSS"),
+                String.join("  ", "O  O", "O  O", "P", "    S"),
+                String.join("  ", "OOOO", "OOOO", "P", "SSSS")
         };
 
+        // Enhanced for-loop to print banner
         for (String line : banner) {
             System.out.println(line);
         }

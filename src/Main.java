79cd9e5bdd848225130801972c70621afd4f40a3
Ashutoshtiwari902 <ashutoshtiwari.9at@gmail.com>
2026-02-23 08:28:20 +0530

    UC4: Stored banner in array and used loop for printing

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
 
 
         System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * ******* ******");
+        System.out.println("*     * *     * *            *");
+        System.out.println("*     * *     * *            *");
+        System.out.println(" *****   *****  *        *****");
     }
 }
